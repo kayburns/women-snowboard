85542951152c99e11bfd2b2e79e83a45604caf8f
Lisa Anne <lisaanne@hachiko.Banatao.Berkeley.EDU>
2018-02-09 18:55:10 -0800

    added training file for blocked image training

---
+++ research/im2txt/train_blocked_image.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+export CUDA_VISIBLE_DEVICES="2"
+
+#export MSCOCO_DIR='/home/lisaanne/lev/data1/caption_bias/models/research/im2txt/im2txt/data/preprocessed' #orig data
+export MSCOCO_DIR='/home/lisaanne/lev/data2/kaylee/caption_bias/models/research/im2txt/im2txt/data/blocked_data' #blocked data
+export INCEPTION_CHECKPOINT='/home/lisaanne//lev/data1/caption_bias/models/research/im2txt/im2txt/data/inception_v3.ckpt'
+export MODEL_DIR='/data/lisaanne/fairness/checkpoints/baseline_train_debug/'
+
+python im2txt/train.py \
+  --input_file_pattern="${MSCOCO_DIR}/train-?????-of-00256" \
+  --inception_checkpoint_file="${INCEPTION_CHECKPOINT}" \
+  --train_dir="${MODEL_DIR}/train" \
+  --train_inception=false \
+  --blocked_image=true \
+  --number_of_steps=1000000
