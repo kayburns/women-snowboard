fade76ca15840f51516fabebf306b6a0d5524039
Lisa Anne <lisaanne@hachiko.Banatao.Berkeley.EDU>
2018-02-12 20:35:13 -0800

    updated example

--- research/im2txt/train_blocked_image.example.sh
+++ research/im2txt/train_blocked_image.example.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-export CUDA_VISIBLE_DEVICES="2"
+export CUDA_VISIBLE_DEVICES="0,1"
 
-export MSCOCO_DIR='/home/lisaanne/lev/data1/caption_bias/models/research/im2txt/im2txt/data/preprocessed' #orig data
-export BLOCKED_MSCOCO_DIR='/home/lisaanne/lev/data1/caption_bias/models/research/im2txt/im2txt/data/blocked_subset_data_avg' #orig data
-#export BLOCKED_MSCOCO_DIR='/home/lisaanne/lev/data1/caption_bias/models/research/im2txt/im2txt/data/blocked_subset_data_avg' #blocked data
+export MSCOCO_DIR='/home/lisaanne/lev/data1/caption_bias/models/research/im2txt/im2txt/data/preprocessed' 
+export BLOCKED_MSCOCO_DIR='/home/lisaanne/lev/data1/caption_bias/models/research/im2txt/im2txt/data/blocked_subset_data_avg' #blocked data
 export INCEPTION_CHECKPOINT='/home/lisaanne//lev/data1/caption_bias/models/research/im2txt/im2txt/data/inception_v3.ckpt'
-export MODEL_DIR='/data/lisaanne/fairness/checkpoints/baseline_train_debug/'
-export INIT_FROM='/data/lisaanne/fairness/checkpoints/LW_train/train/checkpoint'
+export MODEL_DIR='/data/lisaanne/fairness/checkpoints/blocked_image_blockedLW100/'
 
 python im2txt/train.py \
   --input_file_pattern="${BLOCKED_MSCOCO_DIR}/train-?????-of-00256" \
