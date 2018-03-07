6224421e85f0bdaa5734434a4a659be6c0d0b76c
Lisa Anne Hendricks <lisa.a.hendricks@gmail.com>
2018-03-07 04:08:31 +0000

    updated vocab file

--- research/im2txt/im2txt/ops/inputs.py
+++ research/im2txt/im2txt/ops/inputs.py
@@ -31,7 +31,7 @@ except:
 vocab_file = 'im2txt/data/word_counts_fresh.txt'
 #vocab_file = 'im2txt/data/word_counts_fine_tune_2.txt'
 import pdb
-if vocab_file != 'im2txt/data/word_counts.txt':
+if vocab_file != 'im2txt/data/word_counts_fresh.txt':
     print("Wrong vocab file")
     pdb.set_trace()
 loss_weight_words = ['man', 'woman']
