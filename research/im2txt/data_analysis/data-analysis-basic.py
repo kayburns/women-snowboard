4856d03a1d81eb1226975a7980fa3428a51ad3e0
LisaAnne <lisa.a.hendricks@gmail.com>
2018-05-27 12:02:49 -0700

    updated some data analysis

--- research/im2txt/data_analysis/data-analysis-basic.py
+++ research/im2txt/data_analysis/data-analysis-basic.py
@@ -9,18 +9,6 @@ import nltk
 import pdb
 from bias_detection import *
 
-img_2_anno_dict = create_dict_from_list(pickle.load(open(target_train)))
-img_2_anno_dict.update(create_dict_from_list(pickle.load(open(target_test))))
-img_2_anno_dict.update(create_dict_from_list(pickle.load(open(target_val))))
-
-img_2_anno_dict_simple = {}
-for key, value in img_2_anno_dict.iteritems():
-    id = int(key.split('_')[-1].split('.jpg')[0])
-    img_2_anno_dict_simple[id] = {}
-    img_2_anno_dict_simple[id]['male'] = value[0]
-    img_2_anno_dict_simple[id]['female'] = int(not value[0])  
-    assert int(not value[0]) == value[1]
-bias_ids = img_2_anno_dict_simple.keys()
 
 def set_predictions(predictions, anno_ids):
     anno_ids = set(anno_ids)
@@ -29,17 +17,63 @@ def set_predictions(predictions, anno_ids):
         if prediction['image_id'] in anno_ids:
             prediction_subset.append(prediction)
     pred_images = accuracy(prediction_subset, man_word_list_synonyms, woman_word_list_synonyms, img_2_anno_dict_simple)
+    pdb.set_trace()
 
-confident_man = open(base_dir + '/data2/anja/xai/captions/val-confident-man-500new.txt').readlines()
+#get different id sets
+
+img_2_anno_dict = create_dict_from_list(pickle.load(open(target_train)))
+img_2_anno_dict.update(create_dict_from_list(pickle.load(open(target_test))))
+img_2_anno_dict.update(create_dict_from_list(pickle.load(open(target_val))))
+
+predicted_captions = json.load(open(caption_paths[0][1]))
+caption_ids = set([cap['image_id'] for cap in predicted_captions])
+
+img_2_anno_dict_simple = {}
+for key, value in img_2_anno_dict.iteritems():
+    id = int(key.split('_')[-1].split('.jpg')[0])
+    if id in caption_ids:
+        img_2_anno_dict_simple[id] = {}
+        img_2_anno_dict_simple[id]['male'] = value[0]
+        img_2_anno_dict_simple[id]['female'] = int(not value[0])  
+        assert int(not value[0]) == value[1]
+
+bias_ids = img_2_anno_dict_simple.keys()
+    
+confident_man = open(base_dir + '/data2/caption-bias/dataset-splits/val-confident-man.txt').readlines()       
 confident_man = [int(c.strip()) for c in confident_man]
-confident_woman = open(base_dir + '/data2/anja/xai/captions/val-confident-woman-500new.txt').readlines()
+confident_woman = open(base_dir + '/data2/caption-bias/dataset-splits/val-confident-woman.txt').readlines()       
 confident_woman = [int(c.strip()) for c in confident_woman]
 confident_ims = set(confident_man + confident_woman) & set(bias_ids)
+confident_ids = confident_ims
 
+confident_man = open(base_dir + '/data2/anja/xai/captions/val-confident-man-500.txt').readlines()
+confident_man = [int(c.strip()) for c in confident_man]
+confident_woman = open(base_dir + '/data2/anja/xai/captions/val-confident-woman-500.txt').readlines()
+confident_woman = [int(c.strip()) for c in confident_woman]
+confident_ims = set(confident_man + confident_woman) & set(bias_ids)
+balanced_ids = confident_ims
+
+print "For MSCOCO-Biased:"
 for caption_path in caption_paths:
     print "Model name: %s" %caption_path[0]
     predictions = json.load(open(caption_path[1]))
 
-    set_predictions(predictions, confident_ims)
+    set_predictions(predictions, bias_ids)
+    pdb.set_trace() 
+
+print "For MSCOCO-Confident:"
+for caption_path in caption_paths:
+    print "Model name: %s" %caption_path[0]
+    predictions = json.load(open(caption_path[1]))
+
+    set_predictions(predictions, confident_ids)
+    pdb.set_trace() 
+
+print "For MSCOCO-Balanced:"
+for caption_path in caption_paths:
+    print "Model name: %s" %caption_path[0]
+    predictions = json.load(open(caption_path[1]))
+
+    set_predictions(predictions, balanced_ids)
     pdb.set_trace() 
 
