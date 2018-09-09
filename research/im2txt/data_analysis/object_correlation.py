8fbf0031f6e6eb4e1cf837f80edff2a283d16701
LisaAnne <lisa.a.hendricks@gmail.com>
2018-09-09 07:52:34 -0700

    fixed paths for bias_data splits

--- research/im2txt/data_analysis/object_correlation.py
+++ research/im2txt/data_analysis/object_correlation.py
@@ -1,10 +1,25 @@
-from data_analysis_base import AnalysisBaseClass, caption_paths
+from data_analysis_base import AnalysisBaseClass
 
 def get_ids(fname):
     with open(fname, 'r') as f:
         return [int(img_id.split()[0]) for img_id in f.readlines()]
 
 # Crete analysis tools
+# Get caption results
+caption_paths = []
+base_dir = '../final_captions_eccv2018/'
+baseline_ft = ('Baseline-FT', base_dir + 'baseline_ft.json')
+caption_paths.append(baseline_ft)
+uw = ('UpWeight', base_dir + 'upweight.json')
+caption_paths.append(uw)
+balanced = ('Balanced', base_dir + 'balanced.json')
+caption_paths.append(balanced)
+confident =  ('Equalizer w/o ACL', base_dir + 'confident.json')
+caption_paths.append(confident)
+acl = ('Equalizer w/o Confident', base_dir + 'confusion.json')
+caption_paths.append(acl) 
+equalizer = ('Equalizer', base_dir + 'equalizer.json')
+caption_paths.append(equalizer)
 analysis_computer = AnalysisBaseClass(caption_paths)
 
 # Get datasets
@@ -13,7 +28,7 @@ shopping_dev_split_ids = analysis_computer.convert_filenames_to_ids(
     shopping_dev_split
 )
 shopping_test_split = analysis_computer.get_shopping_split(
-    fpath='../data/test.data'
+    fpath='../data/bias_splits/test.data'
 )
 shopping_test_split_ids = analysis_computer.convert_filenames_to_ids(
     shopping_test_split
