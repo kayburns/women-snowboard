a5ac79709738f4c967db52774da28c162f497df0
kayburns <kayleeburns@berkeley.edu>
2018-09-25 10:33:04 -0700

    Update download_and_preprocess_mscoco.sh

--- research/im2txt/im2txt/data/download_and_preprocess_mscoco.sh
+++ research/im2txt/im2txt/data/download_and_preprocess_mscoco.sh
@@ -64,17 +64,17 @@ cd ${SCRATCH_DIR}
 BASE_IMAGE_URL="http://msvocds.blob.core.windows.net/coco2014"
 
 TRAIN_IMAGE_FILE="train2014.zip"
-# download_and_unzip ${BASE_IMAGE_URL} ${TRAIN_IMAGE_FILE}
+download_and_unzip ${BASE_IMAGE_URL} ${TRAIN_IMAGE_FILE}
 TRAIN_IMAGE_DIR="${SCRATCH_DIR}/train2014"
 
 VAL_IMAGE_FILE="val2014.zip"
-# download_and_unzip ${BASE_IMAGE_URL} ${VAL_IMAGE_FILE}
+download_and_unzip ${BASE_IMAGE_URL} ${VAL_IMAGE_FILE}
 VAL_IMAGE_DIR="${SCRATCH_DIR}/val2014"
 
-Download the captions.
-# BASE_CAPTIONS_URL="http://msvocds.blob.core.windows.net/annotations-1-0-3"
+# Download the captions.
+BASE_CAPTIONS_URL="http://msvocds.blob.core.windows.net/annotations-1-0-3"
 CAPTIONS_FILE="captions_train-val2014.zip"
-# download_and_unzip ${BASE_CAPTIONS_URL} ${CAPTIONS_FILE}
+download_and_unzip ${BASE_CAPTIONS_URL} ${CAPTIONS_FILE}
 TRAIN_CAPTIONS_FILE="${SCRATCH_DIR}/annotations/captions_train2014.json"
 VAL_CAPTIONS_FILE="${SCRATCH_DIR}/annotations/captions_val2014.json"
 
