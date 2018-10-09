acd21596d89ef064460c7a8de81ab35c7c7b6a47
Lisa Anne Hendricks <LisaAnne@users.noreply.github.com>
2018-10-09 16:26:02 -0700

    Update train_baseline_ft.sh

--- research/im2txt/train_scripts/train_baseline_ft.sh
+++ research/im2txt/train_scripts/train_baseline_ft.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+#Example inputs:
+#INIT_MODEL_DIR: final_weights_eccv2018/inception_checkpoint
+#BLOCKED_MSCOCO_DIR: im2txt/data/bias_and_blocked
+#INCEPTION_CHECKPOINT: final_weights_eccv2018/inception_checkpoint
+#MODEL: where you would like to save your trained models
+
 python im2txt/train.py \
   --init_from="${INIT_MODEL_DIR}/train" \
   --input_file_pattern="${BLOCKED_MSCOCO_DIR}/train-?????-of-00256" \
