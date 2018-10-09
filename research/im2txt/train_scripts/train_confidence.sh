43cb191d573587a3f831822ae763adef6969f551
Lisa Anne Hendricks <LisaAnne@users.noreply.github.com>
2018-10-09 16:28:04 -0700

    Update train_confidence.sh

--- research/im2txt/train_scripts/train_confidence.sh
+++ research/im2txt/train_scripts/train_confidence.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+#INIT_MODEL_DIR: final_weights_eccv2018/inception_checkpoint
+#BLOCKED_MSCOCO_DIR: im2txt/data/bias_and_blocked
+#INCEPTION_CHECKPOINT: final_weights_eccv2018/inception_checkpoint
+#MODEL_DIR: where you would like to save your trained models
+
 python im2txt/train.py \
   --init_from="${INIT_MODEL_DIR}/train" \
   --input_file_pattern="${BLOCKED_MSCOCO_DIR}/train-?????-of-00256" \
